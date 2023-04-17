c3ec7152cf6ea69f37f9365b69dde47b41598275
Artis84 <lopsta10@gmail.com>
2023-04-17 17:38:19 +0200

    editing server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "src")));
+app.use(express.static(__dirname));
 
 const options = {
     root: path.join(__dirname, "src"),
