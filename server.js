9bd1f72e4822ac437de7510fdb495ca2d043a1a3
Artis84 <lopsta10@gmail.com>
2023-04-17 15:19:23 +0200

    Readed now.json

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "src")));
 
 const options = {
